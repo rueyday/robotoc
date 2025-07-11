baa98fbdb37d221e684374b0b7b8f5d2f18ae08e
Ruey-Day <rueyday1@gmail.com>
2025-07-11 13:52:50 +0800

    update cmake

--- bindings/python/robotoc/__init__.py
+++ bindings/python/robotoc/__init__.py
@@ -1,10 +1,13 @@
+print("init")
 from .robot import *
 from .core import *
 from .cost import *
+print("[rpblem]1")
 from .constraints import *
 # from .dynamics import *
 from .ocp import *
 from .sto import *
+print("[rpblem]")
 from .riccati import *
 from .line_search import *
 from .planner import *
